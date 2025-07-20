e9c95a93eca7d4bcdb7ddf9232c07f555f14064c
Rohama Majeed <rohamamajeed3@gmail.com>
2025-07-20 20:01:42 +0500

    change

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -5,7 +5,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Adjust this to your frontend URL
+    origin: "https://chat-app-frontend-omega-two.vercel.app", // Adjust this to your frontend URL
     credentials: true,
   },
 });

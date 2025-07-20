d5bc0a478fc6fb57a8a4a6bac9ecef886024be9f
Rohama Majeed <rohamamajeed3@gmail.com>
2025-07-20 17:53:10 +0500

    change

--- src/index.js
+++ src/index.js
@@ -15,6 +15,9 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 // const __dirname = path.resolve();
+app.get("/", (req, res) => {
+  res.send("Server is running");  
+});
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(

29283abce50fc7ec21c0c1d26942329324a52d3e
Rohama Majeed <rohamamajeed3@gmail.com>
2026-06-26 11:53:31 +0300

    http node set

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,33 +1,45 @@
 import { Server } from "socket.io";
-import http from "http";
+import http from "node:http";
 import express from "express";
+
 const app = express();
+
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-frontend-omega-two.vercel.app", // Adjust this to your frontend URL
+    origin: "https://chat-app-frontend-omega-two.vercel.app",
     credentials: true,
   },
 });
+
+const userSocketMap = {};
+
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
-const userSocketMap = {};
 
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
+
   const userId = socket.handshake.query.userId;
+
   if (userId) {
     userSocketMap[userId] = socket.id;
     console.log(`User ${userId} connected with socket ID: ${socket.id}`);
   }
+
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
-    delete userSocketMap[userId];
+
+    if (userId) {
+      delete userSocketMap[userId];
+    }
+
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
-
 });
+
 export { io, app, server };

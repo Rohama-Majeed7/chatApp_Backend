74fc60634343cc23a76a141656fd784d9719ff24
Rohama Majeed <rohamamajeed3@gmail.com>
2026-06-26 11:28:48 +0300

    files set

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chat-app-frontend-omega-two.vercel.app",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
